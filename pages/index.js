dfd4c2ccd386b6aa45e2d60a0c91f4a2e149fea3
Faïçal <faycalsawadogo@gmail.com>
2022-02-25 14:54:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Montreal Ballers</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to MTL ballers" />
       </main>
 
       <Footer />
